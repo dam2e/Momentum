e45baaa27e53d26041a397d9487b45fc75571a76
dam2e <ouos2@naver.com>
2020-08-24 23:48:57 +0900

    Docs : 2.5 [연습] 조건문 & 논리연산자

--- run.js
+++ run.js
@@ -1,18 +1,10 @@
-const title = document.querySelector("#title");
+const age = prompt("Hoew old are you?");
 
-// Window Resize
-function handleResize (event){
-    console.log(event);
-    console.log("I have been resized.");
+// 조건문
+if (age >= 18 && age <= 21){
+    console.log("you can drink but you should not");
+} else if (age > 21){
+    console.log("you cant");
+} else {
+    console.log("too young");
 }
-
-window.addEventListener("resize", handleResize); // 이벤트를 다룰 함수 추가
-// handleResize() -> 즉시 실행
-// handleResize -> 윈도우 사이즈 변경 시 실행
-
-function handleClick (){
-    title.style.color = "blue";
-}
-
-// Click Event
-title.addEventListener("click", handleClick);
