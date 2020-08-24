3454c3577c285fb3642f19f6542fe5ed7658ff3c
dam2e <ouos2@naver.com>
2020-08-24 19:19:19 +0900

    Docs : 1.0 [이론] 데이터 구성

--- run.js
+++ run.js
@@ -1,4 +1,25 @@
-let a = 221;
-let b = a - 5;
-a = 4;
+// Array
+let b = a - 5;	const daysOfWeek = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+a = 4;	console.log(daysOfWeek[2]);
 console.log(b,a);
+// Object
+const myInfo = {
+    name: 'Dami',
+    age: 29,
+    gender: 'Male',
+    inHandsome: true,
+    favMovies: ['About Time', 'La La Land', 'Matilda'],
+    favFood: [
+        {
+            name: 'Kimchi',
+            fatty: false
+        },
+        {
+            name: 'Cheese Burger',
+            fatty: true
+        },
+    ]
+};
+myInfo.gender = 'Female';
+console.log(myInfo.gender);
+console.log(myInfo); 
