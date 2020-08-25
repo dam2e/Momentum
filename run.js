61b5ad80f7af252200ce398cac2c8c9d25701f00
dam2e <ouos2@naver.com>
2020-08-26 01:27:00 +0900

    Docs : 2.7 [연습] 조건문 응용2

--- run.js
+++ run.js
@@ -1,16 +1,14 @@
 const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
-/*
-function handleClick (){
-    const hassClass = title.classList.contains(CLICKED_CLASS);
-    if (!hassClass){
-        title.classList.add(CLICKED_CLASS);
-    } else {
-        title.classList.remove(CLICKED_CLASS);
-    }
-}
-*/
+// function handleClick (){
+//     const hassClass = title.classList.contains(CLICKED_CLASS);
+//     if (!hassClass){
+//         title.classList.add(CLICKED_CLASS);
+//     } else {
+//         title.classList.remove(CLICKED_CLASS);
+//     }
+// }
 
 function handleClick (){
     title.classList.toggle(CLICKED_CLASS); // toggle 함수 안에 있는 값을 체크 함.
